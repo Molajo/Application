d32577ea347b67148b90a0d6a6f1e2aa64fa5d8d
AmyStephen <AmyStephen@gmail.com>
2014-04-26 07:36:45 -0500

    For package testing

--- .dev/Bootstrap.php
+++ .dev/Bootstrap.php
@@ -6,16 +6,12 @@
  * @copyright  2014 Amy Stephen. All rights reserved.
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  */
+$base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
+include_once $base . '/vendor/autoload.php';
 include_once __DIR__ . '/CreateClassMap.php';
 
-if (! defined('PHP_VERSION_ID')) {
-    $version = explode('.', phpversion());
-    define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
-}
-
-$base     = substr(__DIR__, 0, strlen(__DIR__) - 5);
 $classmap = array();
-
+/*
 $classmap = createClassMap($base . '/vendor/commonapi/controller', 'CommonApi\\Controller\\');
 $results  = createClassMap($base . '/vendor/commonapi/database', 'CommonApi\\Database\\');
 $classmap = array_merge($classmap, $results);
@@ -29,10 +25,9 @@ $results  = createClassMap($base . '/vendor/commonapi/model', 'CommonApi\\Model\
 $classmap = array_merge($classmap, $results);
 $results  = createClassMap($base . '/vendor/commonapi/resource', 'CommonApi\\Resource\\');
 $classmap = array_merge($classmap, $results);
-
 $results  = createClassMap($base . '/vendor/commonapi/query', 'CommonApi\\Query\\');
 $classmap = array_merge($classmap, $results);
-
+*/
 $results  = createClassMap($base . '/Source/Controller/Translations', 'Molajo\\Controller\\Translations\\');
 $classmap = array_merge($classmap, $results);
 
