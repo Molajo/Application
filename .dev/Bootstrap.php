50af3605727b38333e0bcfdb491c0843251f8fd6
AmyStephen <AmyStephen@gmail.com>
2014-08-08 21:50:01 -0500

    TEsts

--- .dev/Bootstrap.php
+++ .dev/Bootstrap.php
@@ -21,65 +21,6 @@ $classmap = array_merge($classmap, $results);
 $results  = createClassMap($base . '/Source/Controller', 'Molajo\\Controller\\');
 $classmap = array_merge($classmap, $results);
 
-$results  = createClassMap($base . '/Source/Model', 'Molajo\\Model\\');
-$classmap = array_merge($classmap, $results);
-
-$results  = createClassMap($base . '/Source/Plugins/Alias', 'Molajo\\Plugins\\Alias\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Application', 'Molajo\\Plugins\\Application\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Csrftoken', 'Molajo\\Plugins\\Csrftoken\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Customfields', 'Molajo\\Plugins\\Customfields\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Events', 'Molajo\\Plugins\\Events\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Fields', 'Molajo\\Plugins\\Fields\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Footer', 'Molajo\\Plugins\\Footer\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Login', 'Molajo\\Plugins\\Login\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Logout', 'Molajo\\Plugins\\Logout\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Menuitems', 'Molajo\\Plugins\\Menuitems\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Pagetypeapplication', 'Molajo\\Plugins\\Pagetypeapplication\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Pagetypeconfiguration', 'Molajo\\Plugins\\Pagetypeconfiguration\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Pagetypedashboard', 'Molajo\\Plugins\\Pagetypedashboard\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Pagetypeedit', 'Molajo\\Plugins\\Pagetypeedit\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Pagetypegrid', 'Molajo\\Plugins\\Pagetypegrid\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Pagetypeitem', 'Molajo\\Plugins\\Pagetypeitem\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Pagetypelist', 'Molajo\\Plugins\\Pagetypelist\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Pagetypenew', 'Molajo\\Plugins\\Pagetypenew\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Pagination', 'Molajo\\Plugins\\Pagination\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Queryauthorisation', 'Molajo\\Plugins\\Queryauthorisation\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Sites', 'Molajo\\Plugins\\Sites\\');
-$classmap = array_merge($classmap, $results);
-$results  = createClassMap($base . '/Source/Plugins/Status', 'Molajo\\Plugins\\Status\\');
-$classmap = array_merge($classmap, $results);
-ksort($classmap);
-
-$results                                    = createClassMap(
-    $base . '/Source/Resource/Adapter',
-    'Molajo\\Resource\\Adapter\\'
-);
-$classmap                                   = array_merge($classmap, $results);
-$classmap['Molajo\\Resource\\ExtensionMap'] = $base . '/Source/Resource/ExtensionMap.php';
-
-$results  = createClassMap($base . '/Factories', 'Molajo\\Factories\\');
-$classmap = array_merge($classmap, $results);
-
 ksort($classmap);
 
 spl_autoload_register(
